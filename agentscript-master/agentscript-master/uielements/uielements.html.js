60996d9bcece5d39f4fc1fcfa6c5f0dc4d80641f
Andres Barrera <86137176+andresbarrer@users.noreply.github.com>
2025-02-28 21:27:21 -0800

    Delete agentscript-master directory

--- agentscript-master/agentscript-master/uielements/uielements.html.js
+++
@@ -1,8 +0,0 @@
-await fetch('https://code.agentscript.org/uielements/uielements.html')
-    // await fetch('../uielements/uielements.html')
-    .then(response => response.text())
-    .then(html => {
-        // Insert the HTML content for the menu into the DOM
-        document.body.insertAdjacentHTML('afterbegin', html)
-        console.log('uielements.html loaded and parsed')
-    })
